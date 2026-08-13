fe33f0ed5b9678ea1d02328dc4eabf38cf88d32d
ateneaestudioec-svg <ateneaestudioec@gmail.com>
2026-08-13 16:15:24 -0500

    Add files via upload

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -1,7 +1,9 @@
-import imageUrlBuilder from "@sanity/image-url";
+import { createImageUrlBuilder } from "@sanity/image-url";
 import { dataset, projectId } from "../env";
 
-const builder = projectId ? imageUrlBuilder({ projectId, dataset }) : null;
+const builder = projectId
+  ? createImageUrlBuilder({ projectId, dataset })
+  : null;
 
 export function urlForImage(source: object) {
   return builder?.image(source).auto("format").fit("max").url();
