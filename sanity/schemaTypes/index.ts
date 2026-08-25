6d3b0a59562df65e53dd95de15b8ec1bef429293
ateneaestudioec-svg <ateneaestudioec@gmail.com>
2026-08-25 10:11:08 -0500

    Add files via upload

---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,5 @@
+import { blogPost } from "./blogPost";
+import { homePage } from "./homePage";
+import { callToAction, featureGrid3, imageText } from "./homeSections";
+
+export const schemaTypes = [featureGrid3, imageText, callToAction, homePage, blogPost];
